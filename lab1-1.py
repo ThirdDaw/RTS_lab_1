1190b3c2c0f3e0c65af68ddd48eb287cad6fae5c
ThreeDaws <nazarmartyn2000@gmail.com>
2020-05-20 17:29:29 +0300

    Additional Task (dependency Time of N)

--- lab1-1.py
+++ lab1-1.py
@@ -1,10 +1,11 @@
 import random
 import math
 import matplotlib.pyplot as plt
+from datetime import datetime
+import time
 import numpy
 
 harmNumber = 14
-dotNumber = 64
 omegaMax = 2500
 omegaInterval = omegaMax / harmNumber
 
@@ -21,27 +22,42 @@ for i in range(harmNumber):
     phases.append(random.uniform(0, math.pi))
 # print(phase)
 
-mainX = []
-for i in range(dotNumber):
-    currentX = 0
-    for j in range(harmNumber):
-        currentX += ampls[j] * math.sin(omegaInterval * (j + 1) * i + phases[j])
-    mainX.append(currentX)
+alg_time = []
+for i in range(500, 1500, 5):
+    dotNumber = i
+    # time_start = datetime.now()
+    start = time.time()
+    mainX = []
+    for i in range(dotNumber):
+        currentX = 0
+        for j in range(harmNumber):
+            currentX += ampls[j] * math.sin(omegaInterval * (j + 1) * i + phases[j])
+        mainX.append(currentX)
+    # time_finish = datetime.now()
+    finish = time.time()
+    alg_time.append(finish - start)
+print(alg_time)
+# for_schema = [i.microseconds for i in alg_time]
+
+plt.xlabel("Dots")
+plt.ylabel("Time")
+plt.plot(range(500, 1500, 5), alg_time)
+plt.show()
 
 # print(mainX)
 
-mathExp = sum(mainX) / dotNumber
+# mathExp = sum(mainX) / dotNumber
+#
+# diffXpow2 = []
+# for i in range(dotNumber):
+#     diffXpow2.append(math.pow(mainX[i] - mathExp, 2))
+# # disp = sum(diffX) / (dotNumber - 1)
+# disp = sum(diffXpow2) / dotNumber
+#
+# print(mathExp)
+# print(disp)
 
-diffXpow2 = []
-for i in range(dotNumber):
-    diffXpow2.append(math.pow(mainX[i] - mathExp, 2))
-# disp = sum(diffX) / (dotNumber - 1)
-disp = sum(diffXpow2) / dotNumber
-
-print(mathExp)
-print(disp)
-
-plt.xlabel("X")
-plt.ylabel("T")
-plt.plot(mainX)
-plt.show()
+# plt.xlabel("X")
+# plt.ylabel("T")
+# plt.plot(mainX)
+# plt.show()
